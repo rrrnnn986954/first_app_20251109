4a2f94d791adf062e1ec5d2ce0552a53d98a6250
rrrnnn986954 <negima6954@gmail.com>
2025-11-12 14:16:12 +0900

    修正

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
   def index  # indexアクションを定義した
-    @post = "これはコントローラーで定義したインスタンス変数を確認するための文字列です"
+    @posts = Post.all  # すべてのレコードを@postsに代入
   end
 end
